fe8981e758ae1aff6175825c99105860d62bd8ad
Shir Koren <shirko6@gmail.com>
2022-12-14 21:28:44 +0100

    updates server

--- server/server.js
+++ server/server.js
@@ -22,7 +22,8 @@ app.listen(port, () => console.log('server is up and running at port', port))
 app.use(express.static('client/build'));
 
 if( process.env.NODE_ENV === 'production' ){
-    const path = requite('path');
+
+    const path = require('path');
 
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
