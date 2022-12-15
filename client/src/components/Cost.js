1b17412297938f7ebe0d9f349e860bb0df433496
Shir Koren <shirko6@gmail.com>
2022-12-15 17:23:09 +0100

    updated calculation

--- client/src/components/Cost.js
+++ client/src/components/Cost.js
@@ -74,7 +74,7 @@ export default function Cost({ data, setData}) {
       let packagingMaterialCost = `${(material.packagingTotalMaterial * material.packagingPortion )}`
       //console.log("TCL: Cost -> packagingMaterialCost", packagingMaterialCost)
       
-      let totalMaterialCost =`${(parseInt(material.totalMaterialA) || 0) + (parseInt(material.totalMaterialB) || 0) + (parseInt(material.totalMaterialC) || 0) + (parseInt(material.totalMaterialD) || 0)} `
+      let totalMaterialCost =`${(parseInt(materialCostA) || 0) + (parseInt(materialCostB) || 0) + (parseInt(materialCostC) || 0) + (parseInt(materialCostD) || 0)} `
 			//console.log("TCL: Cost -> totalMaterialCost", totalMaterialCost)
       
       let portion =`${(parseInt(material.portionA) || 0) + (parseInt(material.portionB) || 0) + (parseInt(material.portionC) || 0) + (parseInt(material.portionD) || 0)} `
